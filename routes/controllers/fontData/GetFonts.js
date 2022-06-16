3f97e2faeeb40129540e350440ff6d2bd899ea67
ahmadsoran <ahmadsorann@GMAIL.COM>
2022-06-16 19:28:34 +0300

    a

--- routes/controllers/fontData/GetFonts.js
+++ routes/controllers/fontData/GetFonts.js
@@ -2,8 +2,22 @@ import winston from "winston";
 import fontsDatas from "../../../model/Downloads.js";
 
 const GetFonts = async (req, res) => {
+    const { sort } = req.query
+    console.log(sort)
     try {
-        const fonts = await fontsDatas.find();
+        if (sort === 'new') {
+            var fonts = await fontsDatas.find()
+            fonts.reverse()
+
+        } else if (sort === 'old') {
+            var fonts = (await fontsDatas.find())
+        } else if (sort === 'popular') {
+            var fonts = await fontsDatas.find()
+            fonts.sort((a, b) => b.DownloadedTimes - a.DownloadedTimes)
+        } else {
+            var fonts = await fontsDatas.find()
+        }
+
         return res.status(200).json(fonts)
     } catch (error) {
         winston.error(error.message + 'this error happen while getting all fonts');
