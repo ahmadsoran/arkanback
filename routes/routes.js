399f23f596bd955d6e4b7b61c5ab46ad0904b325
ahmadsoran <ahmadsorann@GMAIL.COM>
2022-06-16 05:00:10 +0300

    .

--- routes/routes.js
+++ routes/routes.js
@@ -17,12 +17,7 @@ const storage = multer.diskStorage({})
 let upload = multer({
     storage
 })
-Route.options('/download', (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    res.sendStatus(200);
-})
+
 Route.post("/addAdmin", isAuth, AdminPermission.isAdminOnly, upload.single('uploadImage'), Users.userRegister)
 Route.post("/adminLogin", Users.userLogin)
 Route.get("/getUsers", isAuth, AdminPermission.isAdminOnly, Users.getUserData)
